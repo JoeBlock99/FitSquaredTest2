929f5474f22a6eb99ec9de4aed428f079d271f4a
GDawg4 <garoz.r4@gmail.com>
2018-11-03 11:14:11 -0600

    MasterMind

---
+++ src/main.kt
@@ -0,0 +1,22 @@
+import DifferentClasses.History
+import DifferentClasses.Singleton
+import DifferentClasses.User
+
+fun main(args: Array<String>) {
+    println("Ingrese su edad")
+    var age = readLine()!!
+    println("Ingrese su nivel de ejercicio")
+    var levelOfExercise = readLine()!!
+    println("Ingrese su peso")
+    var weight = readLine()!!
+
+    Singleton.current = User(
+            "",
+            "",
+            age,
+            levelOfExercise,
+            "",
+            weight,
+            History()
+    )
+}
