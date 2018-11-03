4c27039189a95524c97bfcd2d6526c32507e3900
GDawg4 <garoz.r4@gmail.com>
2018-11-03 11:14:32 -0600

    MasterMind

---
+++ src/DifferentClasses/Exercises/SitUps.kt
@@ -0,0 +1,7 @@
+package DifferentClasses.Exercises
+
+abstract class SitUps(name:String,
+                      difficulty:Int,
+                      requirement:String,
+                      type:Int):Exercise(name, difficulty, requirement,type){
+}
